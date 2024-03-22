856fa68eb60f9e1b0cec9ea0cf7a64ff6ae640a7
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2024-03-22 14:11:06 -0300

    Updated screens

--- screens/title_scene.gd
+++ screens/title_scene.gd
@@ -3,3 +3,6 @@ extends Node
 func _on_Timer_timeout():
 	assert(get_tree().change_scene("res://screens/instructions_scene.tscn") == OK)
 
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_select"):
+		assert(get_tree().change_scene("res://screens/instructions_scene.tscn") == OK)
